ee80c66d81f5cd10207a2789c1fd14adc40dfe25
rgri <robert13grimley@gmail.com>
2023-06-26 16:13:27 -0500

    Messing with dependencies

--- flake.nix
+++ flake.nix
@@ -1,28 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    deb.url = "file+file://driver.deb";
+    deb.url = "file:///home/shortcut/git/huion-drivers-flake/driver.deb";
     deb.flake = false;
   };
   outputs = { self, ... }@inp:
     let
       pkgs = inp.nixpkgs.legacyPackages.x86_64-linux;
-      buildStuff = with pkgs; [ autoPatchelfHook dpkg ];
-    in with {
-      inherit pkgs;
-      inherit inp;
-    }; {
-      devShells.x86_64-linux.default = mkShell {
+      buildStuff = with pkgs; [
+        xorg.libXrandr.dev
+        robo3t.out
+        vial.out
+        libusb1.out
+
+        libsForQt5.qt3d.dev
+        libsForQt5.qt5.qtgamepad.dev
+        libsForQt5.qt5.qtvirtualkeyboard.dev
+        qt5.qtbase.dev
+        autoPatchelfHook
+        dpkg
+      ];
+    in with pkgs; {
+      devShells.x86_64-linux.default = pkgs.mkShell {
         packages = buildStuff;
         shellHook = "echo 'you're in a shell";
       };
-      huionDriver = pkgs.mkDerivation {
+      huionDriver = stdenv.mkDerivation {
         name = "Huion Kamvas Driver";
-        version = "1.0";
-        src = ./.;
+        version = "???";
+        src = inp.deb;
         out = src;
+        unpackPhase = "true";
+        dontWrapQtApps = "true";
         buildInputs = buildStuff;
-        nativeBuildInputs = buildstuff;
+        nativeBuildInputs = buildStuff;
         installPhase = ''
           mkdir -p $out
           dpkg -x $src $out
